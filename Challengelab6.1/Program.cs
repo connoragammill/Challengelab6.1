3b17b1722db4071fe8c9bc48b81328d3fc8fb667
connor gammill <connor.gammill@outlook.com>
2024-12-12 13:50:05 -0500

    Add project files.

---
+++ Challengelab6.1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Challengelab6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 2, 1 };
+            int a = 0;
+            foreach (int i in nums)
+            {
+                a ^= i;
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
